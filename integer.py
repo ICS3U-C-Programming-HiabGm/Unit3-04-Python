531341965c30142eb44b7a62e355c0c8659bcaa3
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-03-28 17:50:25 +0000

    Added integer.py

--- integer.py
+++ integer.py
@@ -2,23 +2,23 @@
 # Date: Wed, Feb. 28th
 # This program checks if the number is positive, negative, or just zero
 
+
 def main():
-   # get number of students
-   integer = int(input("Enter an integer: "))
+    # get number of students
+    integer = int(input("Enter an integer: "))
 
-   # check if number if there are too many students
+    # check if number if there are too many students
 
-   if integer > 0:
-      print(f"{integer} is a positive number!")
+    if integer > 0:
+        print(f"{integer} is a positive number!")
+
+    # check if number if there are correct number of students
+    elif integer < 0:
+        print(f"{integer} is a negative number!")
+
+    elif integer == 0:
+        print(f"{integer} is just zero")
 
 
-   # check if number if there are correct number of students
-   elif integer < 0:
-      print(f"{integer} is a negative number!")
-
-   elif integer == 0:
-      print(f"{integer} is just zero")
-
-   
 if __name__ == "__main__":
-   main()
+    main()
